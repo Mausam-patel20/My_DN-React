68e650de7fcc75c7dfb6dffb002e3cde6e2dd93d
Mausam Patel <77777525+Mausam-patel20@users.noreply.github.com>
2025-04-12 17:01:13 -0400

    Code change to Next.js

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+// pages/index.js
+import React from 'react';
+
+export default function Home() {
+    return <h1>Hello from Next.js!</h1>;
+  }  
--- src/components/Greeting.jsx
+++
@@ -1,3 +0,0 @@
-export default function Greeting({ name }) {
-    return <h1>Hello, {name}!</h1>;
-  }  
